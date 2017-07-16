f92c3ed95a287e593382926e6d5b498f50345385
cstwhh <cstwhh@gmail.com>
2017-07-16 19:46:30 +0800

    实现了代码

--- src/BFS.java
+++ src/BFS.java
@@ -1,7 +1,4 @@
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -10,7 +7,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -18,7 +14,9 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class BFS {
-	public final static int DEPTH = 6;
+	public final static int DEPTH = 1;
+	public final static String source = "1";
+	public final static String dest = "10";
 	public static void main(String[] args) throws Exception {		
 		
 		Configuration conf = new Configuration();
@@ -31,18 +29,24 @@ public class BFS {
 		String invertedDataPath = remainingArgs[1]; 
 		String cachePath = remainingArgs[2] + "openNode";
 		for(int i = 1;i < DEPTH; ++i) {
-	        Job invert = Job.getInstance(conf, "BFS");
-	        invert.setJarByClass(BFS.class);
-	
-	        MultipleInputs.addInputPath(invert, new Path(dataPath), TextInputFormat.class,
+	        Job bfs = Job.getInstance(conf, "BFS");
+	        bfs.setJarByClass(BFS.class);
+	        bfs.getConfiguration().set("source", source);
+	        bfs.getConfiguration().set("dest", dest);
+	        String result = bfs.getConfiguration().get("result", "");
+	        if(!result.equals("")) {
+	        	System.out.print(result);
+	        	return;
+	        }
+	        MultipleInputs.addInputPath(bfs, new Path(dataPath), TextInputFormat.class,
 	        		BFSMapper.class);
-	        MultipleInputs.addInputPath(invert, new Path(invertedDataPath), TextInputFormat.class,
+	        MultipleInputs.addInputPath(bfs, new Path(invertedDataPath), TextInputFormat.class,
 	        		BFSMapper.class);
 	        if(i != 1) {
-		        MultipleInputs.addInputPath(invert, new Path(cachePath + (i-1) + "/"), TextInputFormat.class,
+		        MultipleInputs.addInputPath(bfs, new Path(cachePath + (i-1) + "/"), TextInputFormat.class,
 		        		BFSMapper.class);
 	        }
-	        invert.setMapOutputKeyClass(Text.class);
+	        bfs.setMapOutputKeyClass(Text.class);
 	        
 	        String outputPath = cachePath + i + "/";
 	        Path path = new Path(outputPath);
@@ -51,12 +55,12 @@ public class BFS {
 	            fileSystem.delete(path, true);
 	        }  
 	        
-	        FileOutputFormat.setOutputPath(invert, new Path(outputPath));
-	        invert.setOutputFormatClass(TextOutputFormat.class);
-	        invert.setOutputKeyClass(Text.class);
-	        invert.setOutputValueClass(Text.class);
-	        invert.setReducerClass(BFSReducer.class);
-	        System.exit(invert.waitForCompletion(true) ? 0 : 1);
+	        FileOutputFormat.setOutputPath(bfs, new Path(outputPath));
+	        bfs.setOutputFormatClass(TextOutputFormat.class);
+	        bfs.setOutputKeyClass(Text.class);
+	        bfs.setOutputValueClass(Text.class);
+	        bfs.setReducerClass(BFSReducer.class);
+	        System.exit(bfs.waitForCompletion(true) ? 0 : 1);
 		}
 	}
 	public static class BFSMapper extends Mapper<LongWritable, Text, Text, Text> {
@@ -64,23 +68,86 @@ public class BFS {
 	    public void map(LongWritable key, Text value, Context context)
 	          throws IOException, InterruptedException {
       	  	  String[] info = value.toString().split("\t");
-      	  	  if(info == null || info.length < 2) return;
-    		  String actor = info[0];	
-    		  String[] movies = info[1].split("\\|");
-    		  for(String movie: movies) {
-    			  context.write(new Text(movie), new Text(actor));
+    		  String name = info[0];
+    		  String[] children = info[1].split("\\|");
+    		  // all data
+    		  if(info.length == 2) {
+    			  // alldata.source
+    			  if(name.equals(context.getConfiguration().get("source"))) {
+    				  // 按照cachedata.open的发送规则
+	    			  for(String child: children) {
+	    				  context.write(new Text(child), new Text("0\t" + name));
+	    			  }
+	    			  // 更改自己的状态为close，按照cachedata.all的发送规则?
+	    			  context.write(new Text(name), new Text("0\tC" + name));
+    			  }
+    			  // alldata.others
+    			  else {
+    				  context.write(new Text(name), new Text(info[1]));
+    			  }
+    		  }
+    		  // cache data
+    		  else {
+    			  String distance = info[2];
+    			  String status = info[3];
+    			  String parent = info[4];
+    			  if(status.equals("O")) {
+    				  // 按照cachedata.open的发送规则
+	    			  for(String child: children) {
+	    				  context.write(new Text(child), new Text(distance + "\t" + parent + "|" + name));
+	    			  }
+	    			  // 更改自己的状态为close，按照cachedata.all的发送规则
+	    			  status = "C";
+    			  }
+    			  // 按照cachedata.all的发送规则
+    			  context.write(new Text(name), new Text(distance + status + parent));
     		  }
 	    }
 	}
 	public static class BFSReducer extends Reducer<Text,Text,Text,Text> {
 		@Override
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-			List<String> actors = new ArrayList<String>();
+			String children = "";
+			// 存储从自身接收到的cache数据
+			boolean selfHasCached = false;
+			String selfDistance = "";
+			String selfStatus = "";
+			String selfParent = "";
+			// 存储从父节点接收到的cache数据
+			boolean newCache = false;
+			int parentsDistance = Integer.MAX_VALUE;
+			String parentsParent = "";
+			
 			for(Text value: values) {
-				actors.add(value.toString());
+				String[] info = value.toString().split("\t");
+				// self.alldata
+				if(info.length == 0) {
+					children = value.toString();
+				}
+				// self.cachedata
+				else if(info.length == 3) {
+					selfHasCached = true;
+					selfDistance = info[0];
+					selfStatus = info[1];
+					selfParent = info[2];
+				}
+				// parent
+				else if(info.length == 2 && !selfHasCached) {
+					newCache = false;
+					int distance = Integer.valueOf(info[0]);
+					if(distance + 1 < parentsDistance) {
+						parentsDistance = distance + 1;
+						parentsParent = info[1];
+					}
+				}
 			}
-			if(actors.size() == 1) return;
-			context.write(key, new Text(StringUtils.join(actors.toArray(),"|")));
+			// 上一轮已经缓存了，处于close状态
+			if(selfHasCached) {
+				context.write(key, new Text(children + "\t" + selfDistance + "\t" + selfStatus + "\t" + selfParent));
+			}
+			else {
+				context.write(key, new Text(children + "\t" + parentsDistance + "\t" + "O" + "\t" + parentsParent));
+			}
 		}
 	}
 }
