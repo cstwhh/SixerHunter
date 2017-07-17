cc329628da95c8d4a19eb690179ea50971400b98
cstwhh <cstwhh@gmail.com>
2017-07-18 04:07:10 +0800

    will try -inputfiles

--- src/PrefetchOpen.java
+++ src/PrefetchOpen.java
@@ -28,15 +28,15 @@ import org.apache.hadoop.util.StringUtils;
 public class PrefetchOpen {
 	public final static int DEPTH = 13;
 //	public final static String source = "1";public final static String dest = "18";
-//	public final static String source = "Bernardo, Alecia"; public final static String dest = "Boyer, Erica";
-	public final static String source = "Bernardo, Alecia"; public final static String dest = "Boyer, Ericdsadasa";
+	public final static String source = "Bernardo, Alecia"; public final static String dest = "Boyer, Erica";
+//	public final static String source = "Bernardo, Alecia"; public final static String dest = "Boyer, Ericdsadasa";
 	public final static boolean cacheAll = true; 
 	public final static int MAX_CACHE_DEPTH = 5;
 //	public final static int MAX_CACHE_DEPTH = 20;
 	
 	public final static String resultFile = "result";
-	public final static String onlyOpenNodes = "onlyOpenNodes";
-	public final static String allNodes = "allNodes";
+	public final static String onlyOpenNodes = "o";
+	public final static String allNodes = "a";
 	// 表示只发送openNodes之中的节点的children
 //	public static boolean onlyOpenNodes = true;
 	public static void main(String[] args) throws Exception {		
@@ -122,9 +122,14 @@ public class PrefetchOpen {
 	}
 	public static class PrefetchOpenMapper extends Mapper<LongWritable, Text, Text, Text> {
 		private Set<String> openNodes = new HashSet<String>();
+		private Configuration conf;
+		private boolean inOnlyOpenNodesMode = false;
+		private boolean inAllNodesMode = false;
 	    @Override
 	    public void setup(Context context) throws IOException,InterruptedException {
-    		Configuration conf = context.getConfiguration();
+    		conf = context.getConfiguration();
+    		if(conf.get("nodes").equals(onlyOpenNodes)) inOnlyOpenNodesMode = true;
+    		else inAllNodesMode = true;
 	        URI[] openURIs = Job.getInstance(conf).getCacheFiles();
 	        if(openURIs == null) return;
 	        for (URI openURI : openURIs) {
@@ -135,9 +140,7 @@ public class PrefetchOpen {
 		          String openNode = null;
 		          int lineCount = 0;
 		          while ((openNode = fis.readLine()) != null) {
-		        	  openNode = openNode.replace("\t", "");
-//		        	  if(openNode.equals("")) continue;
-		        	  openNodes.add(openNode);
+		        	  openNodes.add(openNode.replace("\t", ""));
 //		        	  System.out.println("addOpenNode: " + openNode.replace("\t", ""));
 		        	  ++ lineCount;
 		          }
@@ -159,13 +162,12 @@ public class PrefetchOpen {
 	    	  //  cachedata.open:	<name children distance parent>
 	          //  cachedata.close:	<name>
 //	    	System.out.println("Map receive: " + value.toString());
-//    	  	  if(value.toString().equals("")) return;
       	  	  String[] info = value.toString().split("\t");
     		  String name = info[0];
     		  // all data
     		  if(info.length == 2) {
     			  // alldata.source
-    			  if(name.equals(context.getConfiguration().get("source"))) {
+    			  if(name.equals(conf.get("source"))) {
     				  // <name,distance parent>，fake自己是被父节点open的
     				  context.write(new Text(name), new Text("0\t" + name));
 	    			  // 必须发送自己<name,children>
@@ -173,11 +175,11 @@ public class PrefetchOpen {
     			  }
     			  // alldata.other <name,children> 
     			  else {
-    				  if(context.getConfiguration().get("nodes").equals(onlyOpenNodes) && openNodes.contains(name)) {
+    				  if(inOnlyOpenNodesMode && openNodes.contains(name)) {
 //    					  System.out.println("Map: OnlyOpenNodes: " + name);
         				  context.write(new Text(name), new Text(info[1]));
     				  }
-    				  else if(context.getConfiguration().get("nodes").equals(allNodes)){
+    				  else if(inAllNodesMode){
 //    					  System.out.println("Map: SendAllNodes: " + name);
         				  context.write(new Text(name), new Text(info[1]));
     				  }
@@ -189,10 +191,10 @@ public class PrefetchOpen {
     			  String distance = info[2];
     			  String parent = info[3];
 				  // 按照cachedata.open的发送规则<child,distance,parent>
-    			  if(name.equals(context.getConfiguration().get("source"))) parent = name;
-    			  else parent = (parent + "|" + name);
-    			  for(String child: children) {
-    				  context.write(new Text(child), new Text(distance + "\t" + parent));
+//    			  if(name.equals(conf.get("source"))) parent = name;else parent = (parent + "|" + name);
+    			  parent = (parent + "|" + name);
+    			  for(int k = 0; k < children.length; ++k) {
+    				  context.write(new Text(children[k]), new Text(distance + "\t" + parent));
     			  }
     			  // 按照cachedata.all的发送规则<name,"<c>"> 
     			  context.write(new Text(name), new Text("<c>"));
@@ -216,7 +218,6 @@ public class PrefetchOpen {
         
 		@Override
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-//			if(key.toString().equals("")) return;
 			// 来自self.alldata的数据
 			String children = "";
 			boolean hasChildren = false;
@@ -228,7 +229,6 @@ public class PrefetchOpen {
 			String parentsParent = "";
 			
 			for(Text value: values) {
-//				if(value.equals(""))	continue;
 //				System.out.println("reduce receive [" + key.toString() + "] " + value);
 				String[] info = value.toString().split("\t");
 				// self.close
@@ -258,7 +258,6 @@ public class PrefetchOpen {
 			else if(isOpening) {
 				if(key.toString().equals(context.getConfiguration().get("dest"))) {
 			        String result = parentsParent + "|" + key.toString();  
-//			        System.out.println("GET: " + result);
 			        Configuration config = context.getConfiguration();
 			        
 		        	FileSystem fs = FileSystem.get(config); 
@@ -277,9 +276,11 @@ public class PrefetchOpen {
 	
 					if(context.getConfiguration().get("writenodes").equals(onlyOpenNodes)) {
 	//					System.out.println("Reduce: OnlyOpenNodes " + key.toString());
-						for(String child: children.split("\\|")) {
+						String[] childs = children.split("\\|");
+//						for(String child: ) {
+		    			 for(int k = 0; k < childs.length; ++k) {
 	//						if(child.length() ==0) continue;
-							mo.write("opendata", new Text(child), new Text(""));
+							mo.write("opendata", new Text(childs[k]), new Text(""));
 						}
 					}
 				}
