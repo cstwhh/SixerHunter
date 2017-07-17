98ac886683f03b983c5480e5e0af8b47b7174047
cstwhh <cstwhh@gmail.com>
2017-07-17 21:39:00 +0800

    等待测试BFSCacheOpen

--- src/BFSCacheOpen.java
+++ src/BFSCacheOpen.java
@@ -19,15 +19,18 @@ import org.apache.hadoop.util.LineReader;
 // 只缓存open节点的全部数据，close节点只保留一个name
 public class BFSCacheOpen {
 	public final static int DEPTH = 12;
-//	public final static String source = "1";
-//	public final static String dest = "10";
-	public final static String source = "Bernardo, Alecia";
-//	public final static String dest = "Boyer, Erica";
-	public final static String dest = "Boyer, Ericdsadasa";
+//	public final static String source = "1";public final static String dest = "8";
+	public final static String source = "Bernardo, Alecia"; public final static String dest = "Boyer, Erica";
+//	public final static String source = "Bernardo, Alecia"; public final static String dest = "Boyer, Ericdsadasa";
+	public final static boolean cacheAll = false; 
+	
 	public final static String resultFile = "result";
 	public static void main(String[] args) throws Exception {		
 		
 		Configuration conf = new Configuration();
+        conf.set("source", source);
+        conf.set("dest", dest);
+        conf.set("resultFile", resultFile);
 		String[] remainingArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
 		if (remainingArgs.length != 3) {
 			System.err.println("Usage: BFS <data> <inverted> <cachePath>");
@@ -46,9 +49,6 @@ public class BFSCacheOpen {
 			long startTime = System.currentTimeMillis();
 	        Job bfs = Job.getInstance(conf, "BFS");
 	        bfs.setJarByClass(BFS.class);
-	        conf.set("source", source);
-	        conf.set("dest", dest);
-	        conf.set("resultFile", resultFile);
 	        // 已经写入结果了，则可以退出
         	if (fs.exists(resultPath)) {
         	    FSDataInputStream fin = fs.open(resultPath);
@@ -68,12 +68,15 @@ public class BFSCacheOpen {
 	        MultipleInputs.addInputPath(bfs, new Path(invertedDataPath), TextInputFormat.class,
 	        		BFSMapper.class);
 	        if(i != 1) {
-		        MultipleInputs.addInputPath(bfs, new Path(cachePath + ((i-1)%2) + "/"), TextInputFormat.class,
-		        		BFSMapper.class);
+		        if(cacheAll) MultipleInputs.addInputPath(bfs, new Path(cachePath + (i-1) + "/"), TextInputFormat.class,BFSMapper.class); 
+		        else MultipleInputs.addInputPath(bfs, new Path(cachePath + ((i-1)%2) + "/"), TextInputFormat.class,BFSMapper.class);
+		        
 	        }
 	        bfs.setMapOutputKeyClass(Text.class);
 	        
-	        String outputPath = cachePath + (i%2) + "/";
+	        String outputPath;
+	        if(cacheAll) outputPath = cachePath + i + "/";
+	        else outputPath = cachePath + (i%2) + "/";
 	        Path path = new Path(outputPath);
 	        FileSystem fileSystem = path.getFileSystem(conf);
 	        if (fileSystem.exists(path)) {  
@@ -95,75 +98,75 @@ public class BFSCacheOpen {
 	    @Override
 	    public void map(LongWritable key, Text value, Context context)
 	          throws IOException, InterruptedException {
-	    	  //  alldata:   <name,children>
-	    	  //  cachedata: <name,children,distance,status,parent>
+	    	  //  alldata:			<name children>
+	    	  //  cachedata.open:	<name children distance parent>
+	          //  cachedata.close:	<name>
       	  	  String[] info = value.toString().split("\t");
     		  String name = info[0];
-    		  String[] children = info[1].split("\\|");
     		  // all data
     		  if(info.length == 2) {
+        		  String[] children = info[1].split("\\|");
     			  // alldata.source
     			  if(name.equals(context.getConfiguration().get("source"))) {
-    				  // 按照cachedata.open的发送规则<child,distance,parent>
+    				  // 按照cachedata.open的发送规则<child,distance parent>
 	    			  for(String child: children) {
 	    				  context.write(new Text(child), new Text("0\t" + name));
 	    			  }
-	    			  // 更改自己的状态为close，按照cachedata.all的发送规则?<name,distance,status,parent> 
-	    			  context.write(new Text(name), new Text("0\tC\t" + name));
+	    			  // 按照cachedata.all的发送规则<name,"<c>"> 
+	    			  context.write(new Text(name), new Text("<c>"));
     			  }
-    			  // alldata.all <name,children>
-    			  context.write(new Text(name), new Text(info[1]));
+    			  // alldata.other <name,children>
+    			  else {
+    				  context.write(new Text(name), new Text(info[1]));
+    			  }
     		  }
-    		  // cache data
-    		  else {
+    		  // cachedata.open
+    		  else if(info.length == 4){
+        		  String[] children = info[1].split("\\|");
     			  String distance = info[2];
-    			  String status = info[3];
-    			  String parent = info[4];
-    			  if(status.equals("O")) {
-    				  // 按照cachedata.open的发送规则<child,distance,parent>
-    				  parent = (parent + "|" + name);
-	    			  for(String child: children) {
-	    				  context.write(new Text(child), new Text(distance + "\t" + parent));
-	    			  }
-	    			  // 更改自己的状态为close，按照cachedata.all的发送规则
-	    			  status = "C";
+    			  String parent = info[3];
+				  // 按照cachedata.open的发送规则<child,distance,parent>
+				  parent = (parent + "|" + name);
+    			  for(String child: children) {
+    				  context.write(new Text(child), new Text(distance + "\t" + parent));
     			  }
-    			  // 按照cachedata.all的发送规则<name,distance,status,parent> 
-    			  context.write(new Text(name), new Text(distance + "\t" + status + "\t" + parent));
+    			  // 按照cachedata.all的发送规则<name,"<c>"> 
+    			  context.write(new Text(name), new Text("<c>"));
     		  }
+    		  // cache.close也按照cache.all的发送规则
+    		  else if(info.length == 1){
+    			  // 按照cachedata.all的发送规则<name,"<c>"> 
+    			  context.write(new Text(name), new Text("<c>"));
+    		  }
+    		  
 	    }
 	}
 	public static class BFSReducer extends Reducer<Text,Text,Text,Text> {
 		@Override
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+			// 来自self.alldata的数据
 			String children = "";
-			// 存储从自身接收到的cache数据
-			boolean selfHasCached = false;
-			String selfDistance = "";
-			String selfStatus = "";
-			String selfParent = "";
-			// 存储从父节点接收到的cache数据
-			boolean newCache = false;
+			// 来自self.close的数据
+			boolean isClosed = false;
+			// 来自parent的数据
+			boolean isOpening = false;
 			int parentsDistance = Integer.MAX_VALUE;
 			String parentsParent = "";
 			
 			for(Text value: values) {
 //				System.out.println("reduce received [" + key.toString() + "] " + value);
 				String[] info = value.toString().split("\t");
+				// self.close
+				if(value.toString().equals("<c>")) {
+					isClosed = true;
+				}
 				// self.alldata
-				if(info.length == 1) {
+				else if(info.length == 1) {
 					children = value.toString();
 				}
-				// self.cachedata:distance,status,parent
-				else if(info.length == 3) {
-					selfHasCached = true;
-					selfDistance = info[0];
-					selfStatus = info[1];
-					selfParent = info[2];
-				}
 				// parent:parent's<distance,parent>
-				else if(info.length == 2 && !selfHasCached) {
-					newCache = true;
+				else if(info.length == 2 && !isClosed) {
+					isOpening = true;
 					int distance = Integer.valueOf(info[0]);
 					if(distance + 1 < parentsDistance) {
 						parentsDistance = distance + 1;
@@ -171,12 +174,12 @@ public class BFSCacheOpen {
 					}
 				}
 			}
-			// 上一轮已经缓存了，处于close状态
-			if(selfHasCached) {
-				context.write(key, new Text(children + "\t" + selfDistance + "\t" + selfStatus + "\t" + selfParent));
+			// close节点
+			if(isClosed) {
+				context.write(key, new Text(""));
 			}
-			// 由unkown变成open状态，如果命中则可以结束了
-			else if(newCache) {
+			// open节点
+			else if(isOpening) {
 				if(key.toString().equals(context.getConfiguration().get("dest"))) {
 			        String result = parentsParent + "|" + key.toString();  
 			        Configuration config = context.getConfiguration();
@@ -192,7 +195,7 @@ public class BFSCacheOpen {
 	        	    fout.close();
 					return;
 				}
-				context.write(key, new Text(children + "\t" + parentsDistance + "\t" + "O" + "\t" + parentsParent));
+				context.write(key, new Text(children + "\t" + parentsDistance + "\t" + parentsParent));
 			}
 		}
 	}
