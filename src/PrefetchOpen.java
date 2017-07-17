711a8fc8289ad18c37f8c5fc85e22b0ae8fcc610
cstwhh <cstwhh@gmail.com>
2017-07-18 01:42:51 +0800

    test prefetch open data

--- src/PrefetchOpen.java
+++ src/PrefetchOpen.java
@@ -1,4 +1,11 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.URI;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
@@ -16,22 +23,29 @@ import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.hadoop.util.LineReader;
+import org.apache.hadoop.util.StringUtils;
 
 // 保存open的数据，alldata只发送open的数据。如果open数据通过setup设置性能反而下降，那么就不再保存open数据
 public class PrefetchOpen {
-	public final static int DEPTH = 12;
-	public final static String source = "1";public final static String dest = "8";
+	public final static int DEPTH = 13;
+//	public final static String source = "1";public final static String dest = "8";
 //	public final static String source = "Bernardo, Alecia"; public final static String dest = "Boyer, Erica";
-//	public final static String source = "Bernardo, Alecia"; public final static String dest = "Boyer, Ericdsadasa";
+	public final static String source = "Bernardo, Alecia"; public final static String dest = "Boyer, Ericdsadasa";
 	public final static boolean cacheAll = false; 
+//	public final static int MAX_CACHE_DEPTH = 6;
+	public final static int MAX_CACHE_DEPTH = 20;
 	
 	public final static String resultFile = "result";
+	public final static String onlyOpenNodes = "onlyOpenNodes";
+	public final static String allNodes = "allNodes";
+	// 表示只发送openNodes之中的节点的children
+//	public static boolean onlyOpenNodes = true;
 	public static void main(String[] args) throws Exception {		
-		
 		Configuration conf = new Configuration();
         conf.set("source", source);
         conf.set("dest", dest);
         conf.set("resultFile", resultFile);
+        conf.set("nodes", onlyOpenNodes);
 		String[] remainingArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
 		if (remainingArgs.length != 3) {
 			System.err.println("Usage: PrefetchOpen <data> <inverted> <cachePath>");
@@ -48,6 +62,9 @@ public class PrefetchOpen {
 		String cachePath = remainingArgs[2];
 		for(int i = 1;i <= DEPTH; ++i) {
 			long startTime = System.currentTimeMillis();
+    	    if(i > MAX_CACHE_DEPTH) {
+    	        conf.set("nodes", allNodes);
+    	    }
 	        Job bfs = Job.getInstance(conf, "PrefetchOpen");
 	        bfs.setJarByClass(PrefetchOpen.class);
 	        // 已经写入结果了，则可以退出
@@ -62,17 +79,22 @@ public class PrefetchOpen {
         	    reader.close();
         	    fin.close();
     	    }
-
-    	    
+	        if(i != 1) {
+	        	String inputPath = cachePath + (i-1);
+	        	if(cacheAll) inputPath = cachePath + (i-1);
+	        	else inputPath = cachePath + ((i-1)%2);
+		        MultipleInputs.addInputPath(bfs, new Path(inputPath + "/part-r-00000"), TextInputFormat.class,PrefetchOpenMapper.class);
+		        if(i <= MAX_CACHE_DEPTH) {
+			        Path openNodePath = new Path(inputPath + "/opendata-r-00000");
+			        if(openNodePath.getFileSystem(conf).exists(openNodePath)) {
+			        	bfs.addCacheFile(openNodePath.toUri());
+			        }
+		        }
+	        }
 	        MultipleInputs.addInputPath(bfs, new Path(dataPath), TextInputFormat.class,
 	        		PrefetchOpenMapper.class);
 	        MultipleInputs.addInputPath(bfs, new Path(invertedDataPath), TextInputFormat.class,
 	        		PrefetchOpenMapper.class);
-	        if(i != 1) {
-		        if(cacheAll) MultipleInputs.addInputPath(bfs, new Path(cachePath + (i-1) + "/part-r-00000"), TextInputFormat.class,PrefetchOpenMapper.class); 
-		        else MultipleInputs.addInputPath(bfs, new Path(cachePath + ((i-1)%2) + "/part-r-00000"), TextInputFormat.class,PrefetchOpenMapper.class);
-		        
-	        }
 	        bfs.setMapOutputKeyClass(Text.class);
 	        
 	        String outputPath;
@@ -83,7 +105,6 @@ public class PrefetchOpen {
 	        if (fileSystem.exists(path)) {  
 	            fileSystem.delete(path, true);
 	        }  
-//	        MultipleOutputs.addNamedOutput(bfs, "cachedata", TextOutputFormat.class, Text.class, Text.class);
 	        MultipleOutputs.addNamedOutput(bfs, "opendata",   TextOutputFormat.class, Text.class, Text.class);
 	        FileOutputFormat.setOutputPath(bfs, new Path(outputPath));
 	        bfs.setOutputFormatClass(TextOutputFormat.class);
@@ -93,16 +114,46 @@ public class PrefetchOpen {
 	        bfs.waitForCompletion(true);
 	        long endTime = System.currentTimeMillis();
 			System.err.println("Depth: " + i + ", Time: " + (endTime - startTime) + "ms");
-	        System.out.print((endTime - startTime) + "ms;");
+	        System.out.println((endTime - startTime) + "ms;");
 		}
 	}
 	public static class PrefetchOpenMapper extends Mapper<LongWritable, Text, Text, Text> {
+		private Set<String> openNodes = new HashSet<String>();
+	    @Override
+	    public void setup(Context context) throws IOException,InterruptedException {
+    		Configuration conf = context.getConfiguration();
+	        URI[] openURIs = Job.getInstance(conf).getCacheFiles();
+	        if(openURIs == null) return;
+	        for (URI openURI : openURIs) {
+	          Path openPath = new Path(openURI.getPath());
+	          String fileName = openPath.toString();
+	          try {
+	        	  BufferedReader fis = new BufferedReader(new FileReader(fileName));
+		          String openNode = null;
+		          int lineCount = 0;
+		          while ((openNode = fis.readLine()) != null) {
+		        	  openNodes.add(openNode.replace("\t", ""));
+//		        	  System.out.println("addOpenNode: " + openNode.replace("\t", ""));
+		        	  ++ lineCount;
+		          }
+		          fis.close();
+//		          if(openNodes.size() > MAX_CACHE_SIZE) onlyOpenNodes = false;
+		          System.out.println("Read " + lineCount + " lines, size: " + openNodes.size());
+//		          System.err.println("file: " + fileName + ", Read " + lineCount + " lines, size: " + openNodes.size() + ", openNodes: " + openNodes.toString());
+		          System.err.println("Read " + lineCount + " lines, size: " + openNodes.size());
+		        } catch (IOException ioe) {
+		          System.err.println("Caught exception while parsing the cached file '"
+		              + StringUtils.stringifyException(ioe));
+		        }
+	        }
+	    }
 	    @Override
 	    public void map(LongWritable key, Text value, Context context)
 	          throws IOException, InterruptedException {
 	    	  //  alldata:			<name children>
 	    	  //  cachedata.open:	<name children distance parent>
 	          //  cachedata.close:	<name>
+//	    	System.out.println("Map receive: " + value.toString());
       	  	  String[] info = value.toString().split("\t");
     		  String name = info[0];
     		  // all data
@@ -110,16 +161,21 @@ public class PrefetchOpen {
         		  String[] children = info[1].split("\\|");
     			  // alldata.source
     			  if(name.equals(context.getConfiguration().get("source"))) {
-    				  // 按照cachedata.open的发送规则<child,distance parent>
-	    			  for(String child: children) {
-	    				  context.write(new Text(child), new Text("0\t" + name));
-	    			  }
-	    			  // 按照cachedata.all的发送规则<name,"<c>"> 
-	    			  context.write(new Text(name), new Text("<c>"));
-    			  }
-    			  // alldata.other <name,children>
-    			  else {
+    				  // <name,distance parent>，fake自己是被父节点open的
+    				  context.write(new Text(name), new Text("0\t" + name));
+	    			  // 必须发送自己<name,children>
     				  context.write(new Text(name), new Text(info[1]));
+    			  }
+    			  // alldata.other <name,children> 
+    			  else {
+    				  if(context.getConfiguration().get("nodes").equals(onlyOpenNodes) && openNodes.contains(name)) {
+//    					  System.out.println("Map: OnlyOpenNodes: " + name);
+        				  context.write(new Text(name), new Text(info[1]));
+    				  }
+    				  else if(context.getConfiguration().get("nodes").equals(allNodes)){
+//    					  System.out.println("Map: SendAllNodes: " + name);
+        				  context.write(new Text(name), new Text(info[1]));
+    				  }
     			  }
     		  }
     		  // cachedata.open
@@ -128,7 +184,8 @@ public class PrefetchOpen {
     			  String distance = info[2];
     			  String parent = info[3];
 				  // 按照cachedata.open的发送规则<child,distance,parent>
-				  parent = (parent + "|" + name);
+    			  if(name.equals(context.getConfiguration().get("source"))) parent = name;
+    			  else parent = (parent + "|" + name);
     			  for(String child: children) {
     				  context.write(new Text(child), new Text(distance + "\t" + parent));
     			  }
@@ -164,7 +221,7 @@ public class PrefetchOpen {
 			String parentsParent = "";
 			
 			for(Text value: values) {
-//				System.out.println("reduce received [" + key.toString() + "] " + value);
+//				System.out.println("reduce receive [" + key.toString() + "] " + value);
 				String[] info = value.toString().split("\t");
 				// self.close
 				if(value.toString().equals("<c>")) {
@@ -192,6 +249,7 @@ public class PrefetchOpen {
 			else if(isOpening) {
 				if(key.toString().equals(context.getConfiguration().get("dest"))) {
 			        String result = parentsParent + "|" + key.toString();  
+//			        System.out.println("GET: " + result);
 			        Configuration config = context.getConfiguration();
 			        
 		        	FileSystem fs = FileSystem.get(config); 
@@ -206,8 +264,15 @@ public class PrefetchOpen {
 					return;
 				}
 				context.write(key, new Text(children + "\t" + parentsDistance + "\t" + parentsParent));
+
+				if(context.getConfiguration().get("nodes").equals(onlyOpenNodes)) {
+//					System.out.println("Reduce: OnlyOpenNodes " + key.toString());
+					for(String child: children.split("\\|")) {
+						mo.write("opendata", new Text(child), new Text(""));
+					}
+				}
 			}
-			mo.write("opendata",new Text("test"), new Text(""));
+			
 		}
         @Override
         protected void cleanup(Context context) throws IOException,InterruptedException{
---
+++ src/prefetchopen.sh
@@ -0,0 +1,4 @@
+hadoop com.sun.tools.javac.Main PrefetchOpen.java 
+jar cf jar/PrefetchOpen.jar PrefetchOpen*.class
+scp jar/PrefetchOpen.jar 2014011446@166.111.227.245:/home/2014011446/PrefetchOpen.jar
+rm *.class
